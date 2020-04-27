b28e4904842ed13dcc2996d45d3e18cd1422568d
Roberto <robertofjv26@gmail.com>
2020-04-27 10:01:27 -0400

    Altareção migrate

--- database/migrations/2020_04_23_010000_create_oms_table.php
+++ database/migrations/2020_04_23_010000_create_oms_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use App\Models\Om\Om;
 
 class CreateOmsTable extends Migration
 {
@@ -19,6 +20,11 @@ class CreateOmsTable extends Migration
             $table->string('sigla',30);
             $table->timestamps();
         });
+
+        return Om::create([
+            'nome' => 'Comando da 12ª Região Militar',
+            'sigla' => 'CMDO 12ªRM',
+        ]);
     }
 
     /**
